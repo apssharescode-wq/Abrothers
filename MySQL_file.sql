18398138ac93e79d834a5529b31ff51ebac1e98e
apssharescode-wq <apssharescode@gmail.com>
2026-09-18 18:02:14 +0545

    Modify Achievers table and update records
    Updated the Achievers table schema and inserted new data.

--- MySQL_file.sql
+++ MySQL_file.sql
@@ -1,22 +1,22 @@
 -- INIT database
 CREATE TABLE Achievers (
-  RollNo INT NOT NULL PRIMARY KEY,
-  Name VARCHAR(10) NOT NULL,
-  Age INT NOT NULL,
-  Stream VARCHAR(10) NOT NULL,
-  DOB DATE
+    RollNo INT NOT NULL PRIMARY KEY,
+    Name VARCHAR(10) NOT NULL,
+    Age INT NOT NULL,
+    Stream VARCHAR(10) NOT NULL,
+    DOB DATE
 );
- 
+
 INSERT INTO Achievers (RollNo, Name, Age, Stream, DOB)
-VALUES (1, 'Sahil', 15, 'Arts', '2011-05-06'),
+VALUES (1, 'Sani', 15, 'Arts', '2011-05-06'),
        (2, 'Sanskriti', 18, 'Science', NULL);
+
 ALTER TABLE Achievers
-ADD Address VARchar(80),
-ADD phone varchar(1230);
+    ADD Address VARCHAR(50),
+    ADD phone VARCHAR(11);
 
 UPDATE Achievers
 SET Address = 'Tokha-4',
-    phone = '9849012330';
+    phone = '9849012338';
 
-
-SELECT * FROM Achievers;
+SELECT * FROM Achievers;   
